b0384158835ede2f623271c98ea42b1bf2f79946
Mochammad Lutfi Bintoro Syaiful <lutfibintoro@gmail.com>
2025-07-01 10:46:57 +0700

    membuat route untuk halaman signup dan merubah base url menjadi halaman signin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegishController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('registrasi.signIn', ['alert_status' => '', 'username' => '', 'alert_message' => '']);
 });
+
+
+// route untuk melakukan sign-up
+Route::get('/sign-up', [RegishController::class, 'signUp']);
+Route::post('/sign-up', [RegishController::class, 'signUp']);
+Route::get('/sign-in/{alert_status}/{username}', [RegishController::class, 'signUpSukses']);
+
+
+// route untuk melakukan sign-in
