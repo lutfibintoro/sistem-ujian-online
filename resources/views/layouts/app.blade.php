a5f5831b8d85b8571a6bf0eab10fda3da48a4e6f
Mochammad Lutfi Bintoro Syaiful <lutfibintoro@gmail.com>
2025-07-01 10:43:45 +0700

    membuat file app.blade.php untuk menjadi dasar layout blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ujian</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <header>@yield('header-content')</header>
+    <main>@yield('main-content')</main>
+    <footer>@yield('footer-content')</footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
